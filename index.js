36d0c848348602a3ca0bfb4b38a8d2cc2aa880aa
dean <deanlloyd64@gmail.com>
2019-12-15 20:03:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+   let SubjectText = document.getElementById("text");
+   SubjectText.innerHTML = "This is really cool!";
+})
