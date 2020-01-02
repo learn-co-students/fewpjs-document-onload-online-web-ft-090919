3797698b68700cee87e84c3212702f659dccfc8b
mplichta <bigcatplichta@gmail.com>
2020-01-02 16:40:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+function updateText() {
+    let text = document.getElementById("text");
+    text.innerHTML = 'This is really cool!';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    updateText()
+  });
